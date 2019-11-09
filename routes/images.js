0c9a1d214796e1cad7eb0adbe0ab4823df16d582
Nisha Bhuva <nishabhuva@Nishas-MacBook-Pro.local>
2019-11-09 13:31:45 -0500

    image route working

--- routes/images.js
+++ routes/images.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path')
 
 /* GET specific image */
-router.get('/:id', function(req, res, next) {
-    var options = {
-        root: path.join(__dirname, 'public'),
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
-        }
-      }
-    res.send("lol");
+router.get('/:name', function(req, res, next) {
+  var options = {
+    root: path.join(__dirname, '/../public'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+
+  var fileName = "images/" + req.params.name + ".jpg"
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
 });
 
 module.exports = router;
